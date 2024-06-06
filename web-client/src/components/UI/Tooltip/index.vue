43b5ed9dcf7412b1b9c6e3b79e3c464559b3db8a
Никита <79268816+ruvemaximus@users.noreply.github.com>
2024-06-06 18:54:19 +0800

    Добавлена клиентская часть приложения (#1)
    * feat(web): добавлена возможность раскрашивать ячейки
    
    * feat: добавлена возможность выбора цвета для окрашивания
    
    * improve(backend): добавлен volume для БД

---
+++ web-client/src/components/UI/Tooltip/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="absolute left-14 p-4 rounded-lg bg-white border-2 border-slate-200 shadow"
+  >
+    <h4 class="font-extrabold">
+      <slot name="header" />
+    </h4>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ web-client/src/components/UI/index.ts
@@ -0,0 +1,5 @@
+import Button from "@/components/UI/Button.vue";
+import Navbar from "@/components/UI/Navbar.vue";
+import Tooltip from "@/components/UI/Tooltip/index.vue";
+
+export { Button, Navbar, Tooltip };
